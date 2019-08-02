40cd10a14cea6b353833598a2da1f40c0b493d3e
chicrock <jungsungrak@gmail.com>
2019-08-02 19:50:28 +0900

    Try my First flutter app

---
+++ README.md
@@ -0,0 +1,19 @@
+# Do Flutter
+
+Try flutter !!
+
+## Install packages
+
+```yaml
+# In pubspec.yaml
+dependencies:
+  flutter:
+    sdk: flutter
+cupertino_icons: ^0.1.2
++   english_words: ^3.1.0
+```
+
+```bash
+# Type in cli. This command do install modules via the pubspec.yaml
+flutter pub get
+```
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+        title: 'MainApp',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('first word app'.toUpperCase()),
+            ),
+            body: Center(child: Text(wordPair.asUpperCase))));
+  }
+}
