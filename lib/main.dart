bb6ca9e1e6bdc67b8132a9fb6a36f1d53d9a74df
ChicRock <jungsungrak@gmail.com>
2019-08-15 20:26:21 +0900

    Add splash change widget on auth state change (login and mainpage)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:doflutter/screens/login.dart';
+import 'package:doflutter/screens/main_page.dart';
 import 'package:doflutter/stores/joinOrLogin.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,8 +12,22 @@ class MyApp extends StatelessWidget {
   // for check the provider working well in children widgets
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: ChangeNotifierProvider<JoinOrLogin>.value(
-            value: JoinOrLogin(), child: AuthScreen()));
+    return MaterialApp(home: Splash());
+  }
+}
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<FirebaseUser>(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return ChangeNotifierProvider<JoinOrLogin>.value(
+                value: JoinOrLogin(), child: AuthScreen());
+          } else {
+            return MainPage(email: snapshot.data.email);
+          }
+        });
   }
 }
